607dffcf5fdae19c18bdefe20160a282ac97fbc7
PC-0506602 <viet09cntt@gmail.com>
2026-03-05 08:21:58 +0700

    Restore missing private methods in Manager class

--- inc/core/breadcrumbs/class-breadcrumbs-manager.php
+++ inc/core/breadcrumbs/class-breadcrumbs-manager.php
@@ -126,4 +126,79 @@ class OceanWP_Breadcrumbs_Manager {
 		$renderer = new OceanWP_Breadcrumbs_Renderer( $crumbs, $args );
 		return $renderer->render();
 	}
+
+	/**
+	 * Determine which provider to use.
+	 */
+	private function determine_provider( $args ) {
+		$provider_class = '';
+
+		if ( function_exists( 'is_woocommerce' ) && is_woocommerce() ) {
+			$provider_class = 'OceanWP_Breadcrumbs_WooCommerce_Provider';
+		} elseif ( is_singular() ) {
+			$provider_class = 'OceanWP_Breadcrumbs_Singular_Provider';
+		} elseif ( is_search() ) {
+			$provider_class = 'OceanWP_Breadcrumbs_Search_Provider';
+		} elseif ( is_404() ) {
+			$provider_class = 'OceanWP_Breadcrumbs_404_Provider';
+		} elseif ( is_archive() ) {
+			$provider_class = 'OceanWP_Breadcrumbs_Archive_Provider';
+		}
+
+		$provider_class = apply_filters( 'oceanwp_breadcrumbs_provider_class', $provider_class, $args );
+
+		if ( ! empty( $provider_class ) && class_exists( $provider_class ) ) {
+			return new $provider_class( $args );
+		}
+
+		return null;
+	}
+
+	/**
+	 * Get labels.
+	 */
+	private function get_labels( $custom_labels ) {
+		$defaults = array(
+			'aria_label'     => esc_attr_x( 'Breadcrumbs', 'breadcrumbs aria label', 'oceanwp' ),
+			'home'           => get_theme_mod( 'ocean_breadcrumb_translation_home', esc_html__( 'Home', 'oceanwp' ) ),
+			'error_404'      => get_theme_mod( 'ocean_breadcrumb_translation_error', esc_html__( '404 Not Found', 'oceanwp' ) ),
+			'archives'       => esc_html__( 'Archives', 'oceanwp' ),
+			'search'         => get_theme_mod( 'ocean_breadcrumb_translation_search', esc_html__( 'Search results for', 'oceanwp' ) ),
+			'paged'          => esc_html__( 'Page %s', 'oceanwp' ),
+		);
+
+		return apply_filters( 'breadcrumb_trail_labels', wp_parse_args( $custom_labels, $defaults ) );
+	}
+
+	/**
+	 * Get home crumbs.
+	 */
+	private function get_home_crumbs( $args ) {
+		$home_crumbs = array();
+		
+		$setting = get_theme_mod( 'ocean_breadcrumb_home_item', 'icon' );
+		$label   = $args['labels']['home'];
+		$icon    = '';
+
+		if ( 'icon' === $setting || is_customize_preview() ) {
+			$icon = function_exists( 'oceanwp_icon' ) ? oceanwp_icon( 'home', false ) : '';
+		}
+
+		if ( 'icon' === $setting && ! is_customize_preview() ) {
+			$label = $icon;
+		} elseif ( is_customize_preview() ) {
+			$text_class = ( 'icon' === $setting ) ? ' has-icon' : '';
+			$label = $icon . '<span class="breadcrumb-home' . $text_class . '">' . $label . '</span>';
+		}
+
+		// Network home
+		if ( is_multisite() && ! is_main_site() && ! empty( $args['network'] ) ) {
+			$home_crumbs[] = new OceanWP_Breadcrumbs_Crumb( $label, network_home_url() );
+		}
+
+		// Site home
+		$home_crumbs[] = new OceanWP_Breadcrumbs_Crumb( $label, home_url() );
+
+		return $home_crumbs;
+	}
 }
